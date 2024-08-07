2b4812ef2241e6490b4011f7dc4cdd3beea6e4cd
Teemsploit <53855235+Teemsploit@users.noreply.github.com>
2024-08-07 09:15:40 +0100

    Update admin_system.py

--- admin_system.py
+++ admin_system.py
@@ -7,6 +7,7 @@ import json
 import uuid
 import re
 import logging
+import importlib.util
 
 APPDATA = os.getenv('APPDATA')
 LOG_FILE_PATH = os.path.join(APPDATA, 'situation.log')
@@ -18,10 +19,25 @@ kernel32 = ctypes.windll.kernel32
 
 commands = {}
 
+plugins_directory = os.path.join(APPDATA, "situation_plugins")
+
 def split(input_str, sep=" "):
     if not input_str:
         return []
     return input_str.split(sep)
+
+def load_plugins():
+    if not os.path.exists(plugins_directory):
+        os.makedirs(plugins_directory)
+    
+    for filename in os.listdir(plugins_directory):
+        if filename.endswith('.py'):
+            plugin_path = os.path.join(plugins_directory, filename)
+            spec = importlib.util.spec_from_file_location(filename[:-3], plugin_path)
+            plugin_module = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(plugin_module)
+            plugin_name = filename[:-3]
+            commands[plugin_name] = plugin_module.main
 
 def help_command(args):
     print("Available commands:")
@@ -30,7 +46,15 @@ def help_command(args):
 
 commands['help'] = help_command
 
+def show_console():
+    kernel32.AllocConsole()
+    console_handle = kernel32.GetConsoleWindow()
+    user32.ShowWindow(console_handle, SW_SHOW)
+    print("Console shown.")
+
 def main():
+    show_console()
+    load_plugins()
     print("Welcome to Situation. Type 'help' for a list of commands.")
     while True:
         try:
